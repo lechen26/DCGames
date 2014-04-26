3cbb2b74ac89f81f8d6e48481f5db256b1ab6c55
David Feldstein <david.f@fiverr.com>
2014-04-26 20:29:25 +0300

    [UPDATE] added 2048 colors like the real game

--- src/view/State.java
+++ src/view/State.java
@@ -3,6 +3,7 @@ package view;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.events.PaintListener;
+import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
@@ -13,7 +14,7 @@ public class State extends Canvas {
 	public State(Composite parent, int style) {
 		super(parent,style);		
 		value=0;
-		Font font = new Font(getDisplay(), getFont().getFontData()[0].getName(),14,SWT.BORDER);
+		Font font = new Font(getDisplay(), getFont().getFontData()[0].getName(),26,SWT.BORDER);
 		setFont(font);		
 		
 		addPaintListener(new PaintListener() {
@@ -37,33 +38,52 @@ public class State extends Canvas {
 	private void changeBackground(int val) {
 		switch(val) {
 		case 0: 
-			setBackground(getDisplay().getSystemColor(SWT.COLOR_DARK_GRAY));
+			Color bgColor0 = new Color(getDisplay(),238, 228, 218);
+			setBackground(bgColor0);
 			break;
 		case 2:
-			setBackground(getDisplay().getSystemColor(SWT.COLOR_GRAY));
+			Color bgColor2 = new Color(getDisplay(),238,228,218);
+			setBackground(bgColor2);	
 			break;
 		case 4:
-			setBackground(getDisplay().getSystemColor(SWT.COLOR_CYAN));
+			Color bgColor4 = new Color(getDisplay(),237,224,200);
+			setBackground(bgColor4);	
 			break;
 		case 8: 
-			setBackground(getDisplay().getSystemColor(SWT.COLOR_YELLOW));
+			Color bgColor8 = new Color(getDisplay(),242, 177, 121);
+			setBackground(bgColor8);	
 			break;
 		case 16:
-			setBackground(getDisplay().getSystemColor(SWT.COLOR_DARK_MAGENTA));
+			Color bgColor16 = new Color(getDisplay(),245, 149, 99);
+			setBackground(bgColor16);	
+			break;
+		case 32:
+			Color bgColor32 = new Color(getDisplay(),246, 124, 95);
+			setBackground(bgColor32);	
 			break;
 		case 64:
-			setBackground(getDisplay().getSystemColor(SWT.COLOR_RED));
+			Color bgColor64 = new Color(getDisplay(),246, 94, 59);
+			setBackground(bgColor64);	
 			break;
 		case 128:
-			setBackground(getDisplay().getSystemColor(SWT.COLOR_CYAN));
+			Color bgColor128 = new Color(getDisplay(),237, 207, 114);
+			setBackground(bgColor128);	
 			break;
 		case 256:
+			Color bgColor256 = new Color(getDisplay(),237, 204, 97);
+			setBackground(bgColor256);	
 			break;
 		case 512:
+			Color bgColor512 = new Color(getDisplay(),237, 200, 80);
+			setBackground(bgColor512);	
 			break;
 		case 1024:
+			Color bgColor1024 = new Color(getDisplay(),237, 197, 63);
+			setBackground(bgColor1024);	
 			break;
 		case 2048:
+			Color bgColor2048 = new Color(getDisplay(),237, 194, 46);
+			setBackground(bgColor2048);	
 			break;
 	}
 			
