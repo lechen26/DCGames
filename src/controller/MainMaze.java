abd0a6ab425db74c273b0a25eb8a0bf3654811f0
David Feldstein <david.f@fiverr.com>
2014-04-26 14:38:50 +0300

    [ADD] aded mainmaze

---
+++ src/controller/MainMaze.java
@@ -0,0 +1,22 @@
+package controller;
+
+import view.ViewMaze;
+import model.ModelMaze;
+
+public class MainMaze {
+
+	public static void main(String[] args) {
+		ModelMaze m = new ModelMaze();
+		ViewMaze ui = new ViewMaze();
+		Presenter p = new Presenter(m,ui);
+		m.addObserver(p);
+		ui.addObserver(p);			
+		//Thread th = new Thread(ui);
+		//th.start();
+		ui.run();
+		//m.initializeBoard();
+
+
+	}
+
+}
