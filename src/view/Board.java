edf648651bc580e78cd70cb0e1632b792e56252a
chen <chen@Lap-CL.(none)>
2014-05-03 11:27:26 +0300

    improve Maze appearnce- no gaps

--- src/view/Board.java
+++ src/view/Board.java
@@ -3,13 +3,15 @@ package view;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.DisposeEvent;
 import org.eclipse.swt.events.DisposeListener;
+import org.eclipse.swt.events.PaintEvent;
+import org.eclipse.swt.events.PaintListener;
+import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
 
 
-public class Board extends Canvas { 
+public class Board extends Composite { 
 	
 	int[][] boardData;
 	int rows,cols;
@@ -21,30 +23,31 @@ public class Board extends Canvas {
 	public Board(Composite parent, int style, int rows, int cols, int stateStyle) {
 		super(parent,style);	
 		boardData = new int[rows][cols];
-		setLayout(new GridLayout(cols,true));
-		states = new State  [rows][cols];
+		GridLayout grid = new GridLayout(cols,false);
+		grid.horizontalSpacing=0;
+		grid.verticalSpacing=0;
+		setLayout(grid);		
+		states = new State[rows][cols];
 		for(int i=0;i< boardData.length; ++i)
 		{
-				for(int j=0; j < boardData[0].length; ++j)
+				for(int j=0; j < boardData[0].length; j++)
 				{					
 					states[i][j] = new State(Board.this,stateStyle);
 					states[i][j].setLayoutData(new GridData(SWT.FILL,SWT.FILL,true,true));
 					states[i][j].setValue(boardData[i][j]);
 				}	
 				
-		}			
-		
+		}		
 		this.addDisposeListener(new DisposeListener() {
 			@Override
 			public void widgetDisposed(DisposeEvent e) {
 				for(int i=0;i< boardData.length; ++i)
 				{
-					for(int j=0; j < boardData[0].length; ++j)
-					{							
-						states[i][j].dispose();
-					}
-				}	}
-			});
+					for(int j=0; j < boardData[0].length; ++j)								
+						states[i][j].dispose();					
+				}
+			}
+		});
 	}
 	
 	/*
@@ -54,7 +57,7 @@ public class Board extends Canvas {
 		for(int i=0;i< boardData.length; ++i)
 		{
 			for(int j=0; j < boardData[0].length; ++j)
-			{
+			{								
 				boardData[i][j] = board[i][j];				
 				states[i][j].setValue(boardData[i][j]);
 			}
