cdeac613a85b9222f74f0838c961cc4c70f8c0f7
lechen26 <levichen24@gmail.com>
2014-05-04 18:40:09 +0300

    update Mouse Event (Click Release) movement

--- src/view/State.java
+++ src/view/State.java
@@ -6,120 +6,117 @@ import org.eclipse.swt.events.PaintListener;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
 
-public class State extends Canvas {		
-	int value=0;
-	
+public class State extends Canvas {
+	int value = 0;
+
 	public State(Composite parent, int style) {
-		super(parent,style);				
-		Font font = new Font(getDisplay(), getFont().getFontData()[0].getName(),25,SWT.NONE);
-		setFont(font);				
+		super(parent, style);
+		Font font = new Font(getDisplay(), getFont().getFontData()[0].getName(), 25, SWT.NONE);
+		setFont(font);
 		addPaintListener(new PaintListener() {
 			@Override
-			public void paintControl(PaintEvent e) {				
+			public void paintControl(PaintEvent e) {
 				int stringWidth = e.gc.getFontMetrics().getAverageCharWidth();
-				int x = (getSize().x)/2  - (("" + value).length())*stringWidth/2;
-				int y = (getSize().y)/2 - (("" + value).length())*stringWidth/2;
-				if ((value > 0 ) && (value != -1) && (value != 1) && (value !=23))
+				int x = (getSize().x) / 2 - (("" + value).length()) * stringWidth / 2;
+				int y = (getSize().y) / 2 - (("" + value).length()) * stringWidth / 2;
+				if ((value > 0) && (value != -1) && (value != 1) && (value != 23))
 					e.gc.drawString("" + value, x, y);
-				if (value == 1)
-				{					
-					x=((Canvas) e.widget).getBounds().width;
-					y=((Canvas) e.widget).getBounds().height;
-					Image mouse = new Image(getDisplay(),"resources/mouse.jpg");
-					Image scaledMouse = new Image(getDisplay(),mouse.getImageData().scaledTo(x, y));
-					e.gc.drawImage(scaledMouse,0,0);					
+				if (value == 1) {
+					x = ((Canvas) e.widget).getBounds().width;
+					y = ((Canvas) e.widget).getBounds().height;
+					Image mouse = new Image(getDisplay(), "resources/mouse.jpg");
+					Image scaledMouse = new Image(getDisplay(), mouse.getImageData().scaledTo(x, y));
+					e.gc.drawImage(scaledMouse, 0, 0);
 				}
-				if (value == 23)
-				{
-					x=((Canvas) e.widget).getBounds().width;
-					y=((Canvas) e.widget).getBounds().height;
-					Image cheeze = new Image(getDisplay(),"resources/cheeze.jpg");
-					Image scaledMouse = new Image(getDisplay(),cheeze.getImageData().scaledTo(x, y));
-					e.gc.drawImage(scaledMouse,0,0);
+				if (value == 23) {
+					x = ((Canvas) e.widget).getBounds().width;
+					y = ((Canvas) e.widget).getBounds().height;
+					Image cheeze = new Image(getDisplay(), "resources/cheeze.jpg");
+					Image scaledMouse = new Image(getDisplay(), cheeze.getImageData().scaledTo(x, y));
+					e.gc.drawImage(scaledMouse, 0, 0);
 				}
-			}			
+			}
 		});
 	}
-	
 
 	public void setValue(int value) {
-		this.value=value;
+		this.value = value;
 		changeBackground(value);
 		redraw();
-		
+
 	}
-	
-	
+
 	public void changeBackground(int val) {
-		switch(val) {
-		case -2: 
-			Color bgColor0 = new Color(getDisplay(),238, 228, 218);
+		switch (val) {
+		case -2:
+			Color bgColor0 = new Color(getDisplay(), 238, 228, 218);
 			setBackground(bgColor0);
 			break;
 		case 2:
-			Color bgColor2 = new Color(getDisplay(),238,228,218);
-			setBackground(bgColor2);	
+			Color bgColor2 = new Color(getDisplay(), 238, 228, 218);
+			setBackground(bgColor2);
 			break;
 		case 4:
-			Color bgColor4 = new Color(getDisplay(),237,224,200);
-			setBackground(bgColor4);	
+			Color bgColor4 = new Color(getDisplay(), 237, 224, 200);
+			setBackground(bgColor4);
 			break;
-		case 8: 
-			Color bgColor8 = new Color(getDisplay(),242, 177, 121);
-			setBackground(bgColor8);	
+		case 8:
+			Color bgColor8 = new Color(getDisplay(), 242, 177, 121);
+			setBackground(bgColor8);
 			break;
 		case 16:
-			Color bgColor16 = new Color(getDisplay(),245, 149, 99);
-			setBackground(bgColor16);	
+			Color bgColor16 = new Color(getDisplay(), 245, 149, 99);
+			setBackground(bgColor16);
 			break;
 		case 32:
-			Color bgColor32 = new Color(getDisplay(),246, 124, 95);
-			setBackground(bgColor32);	
+			Color bgColor32 = new Color(getDisplay(), 246, 124, 95);
+			setBackground(bgColor32);
 			break;
 		case 64:
-			Color bgColor64 = new Color(getDisplay(),246, 94, 59);
-			setBackground(bgColor64);	
+			Color bgColor64 = new Color(getDisplay(), 246, 94, 59);
+			setBackground(bgColor64);
 			break;
 		case 128:
-			Color bgColor128 = new Color(getDisplay(),237, 207, 114);
-			setBackground(bgColor128);	
+			Color bgColor128 = new Color(getDisplay(), 237, 207, 114);
+			setBackground(bgColor128);
 			break;
 		case 256:
-			Color bgColor256 = new Color(getDisplay(),237, 204, 97);
-			setBackground(bgColor256);	
+			Color bgColor256 = new Color(getDisplay(), 237, 204, 97);
+			setBackground(bgColor256);
 			break;
 		case 512:
-			Color bgColor512 = new Color(getDisplay(),237, 200, 80);
-			setBackground(bgColor512);	
+			Color bgColor512 = new Color(getDisplay(), 237, 200, 80);
+			setBackground(bgColor512);
 			break;
 		case 1024:
-			Color bgColor1024 = new Color(getDisplay(),237, 197, 63);
-			setBackground(bgColor1024);	
+			Color bgColor1024 = new Color(getDisplay(), 237, 197, 63);
+			setBackground(bgColor1024);
 			break;
 		case 2048:
-			Color bgColor2048 = new Color(getDisplay(),237, 194, 46);
-			setBackground(bgColor2048);	
+			Color bgColor2048 = new Color(getDisplay(), 237, 194, 46);
+			setBackground(bgColor2048);
 			break;
 		case 0:
 			setBackground(getDisplay().getSystemColor(SWT.COLOR_WHITE));
-			break;		
+			break;
 		case -1:
 			setBackground(getDisplay().getSystemColor(SWT.COLOR_BLACK));
 			break;
-			/*
+		// case 1:
+
+		/*
 		case 1:
-			//setBackground(getDisplay().getSystemColor(SWT.COLOR_GREEN));			
-			setBackgroundImage(img);			
-			break;*/
-		case 23: 
+		//setBackground(getDisplay().getSystemColor(SWT.COLOR_GREEN));			
+		setBackgroundImage(img);			
+		break;*/
+		case 23:
 			setBackground(getDisplay().getSystemColor(SWT.COLOR_RED));
 		}
 	}
-	
+
 	public State getState() {
 		return this;
 	}
--- src/view/ViewMaze.java
+++ src/view/ViewMaze.java
@@ -20,439 +20,414 @@ import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.swt.widgets.MessageBox;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Tracker;
 
-public class ViewMaze extends Observable implements View,Runnable {
+public class ViewMaze extends Observable implements View, Runnable {
 
 	Display display;
 	Shell shell;
-	Board board;	
-	int userCommand=0;
-	int rows,cols;
-	int pressed=0;
-	int horizental=0;
-	int vertical=0;
+	Board board;
+	int userCommand = 0;
+	int rows, cols;
+	int pressed = 0;
+	int horizental = 0;
+	int vertical = 0;
 	Label score;
-	boolean rightPressed=false;
-	boolean leftPressed=false;
-	boolean upPressed=false;
-	boolean downPressed=false;
-	
+	boolean rightPressed = false;
+	boolean leftPressed = false;
+	boolean upPressed = false;
+	boolean downPressed = false;
+
 	public ViewMaze(int rows, int cols) {
-		this.rows=rows;
-		this.cols=cols;
+		this.rows = rows;
+		this.cols = cols;
 	}
-	
-	private void initComponents(){
-		int scr=0;
+
+	private void initComponents() {
+		int scr = 0;
 		display = new Display();
 		shell = new Shell(display);
-		shell.setLayout(new GridLayout(2,false));
+		shell.setLayout(new GridLayout(2, false));
 		shell.setSize(600, 500);
 		shell.setText("Levi and Duvid's Maze Game");
-	    
-		//Defines menu game
+
+		// Defines menu game
 		initializeMenu();
-		
-	    //Defines the Score label
-	    score = new Label(shell, SWT.BORDER);
+
+		// Defines the Score label
+		score = new Label(shell, SWT.BORDER);
 		score.setText("Score:      " + scr);
-		
-		
-		//Defines the board game
+
+		// Defines the board game
 		initializeBoard();
-		
-		//Defines the buttons of the game
+
+		// Defines the buttons of the game
 		initializeButtons();
-		
-		//Defines Key Listener
+
+		// Defines Key Listener
 		shell.forceFocus();
 		shell.addKeyListener(new KeyListener() {
-			
+
 			@Override
 			public void keyReleased(KeyEvent e) {
-				//Switch case on the Key Release 
+				// Switch case on the Key Release
 				switch (e.keyCode) {
-					case SWT.ARROW_UP:		
-						upPressed = false;
-						pressed--;
-						break;
-					case SWT.ARROW_DOWN:
-						downPressed = false;
-						pressed--;
-						break;
-					case SWT.ARROW_RIGHT:		
-						rightPressed = false;
-						pressed--;
-						break;
-					case SWT.ARROW_LEFT:	
-						leftPressed = false;
-						pressed--;
-						break;
-					default:
-						break;
-				}		
+				case SWT.ARROW_UP:
+					upPressed = false;
+					pressed--;
+					break;
+				case SWT.ARROW_DOWN:
+					downPressed = false;
+					pressed--;
+					break;
+				case SWT.ARROW_RIGHT:
+					rightPressed = false;
+					pressed--;
+					break;
+				case SWT.ARROW_LEFT:
+					leftPressed = false;
+					pressed--;
+					break;
+				default:
+					break;
+				}
 				setChanged();
-				userCommand=e.keyCode;
+				userCommand = e.keyCode;
 				// support diagonal moves
 				if (pressed == 0) {
 					notifyObservers("" + horizental + "," + vertical);
-					horizental=0;
-					vertical=0;
+					horizental = 0;
+					vertical = 0;
 				}
 			}
-			
+
 			@Override
-			public void keyPressed(KeyEvent e) {	
+			public void keyPressed(KeyEvent e) {
 				// switch case for the key pressed
 				switch (e.keyCode) {
-					case SWT.ARROW_UP:		
-						vertical++;
-						if (!upPressed){
-							pressed++;
-							upPressed=true;
-						}
-						break;
-					case SWT.ARROW_DOWN:
-						vertical--;
-						if (!downPressed){
-							pressed++;
-							downPressed=true;
-						}
-						break;
-					case SWT.ARROW_RIGHT:		
-						horizental++;
-						if (!rightPressed){
-							pressed++;
-							rightPressed=true;
-						}
-						break;
-					case SWT.ARROW_LEFT:	
-						horizental--;
-						if (!leftPressed){
-							pressed++;
-							leftPressed=true;
-						}
-						break;
-				
-					default:
-						break;
-				}		
-			}
-		});		
-		 //mouse event listener
-		for (int i=0 ; i < rows ; i++)
-			for (int j=0 ; j< cols ; j++)
-			{
-//				if (board.states[i][j].value == 1){
-					 board.states[i][j].addMouseListener(new MouseListener() {
-						int mouseCurXpos;
-						int mouseCurYpos;
-						int mouseNewXpos;
-						int mouseNewYpos;
-			
-						public void mouseDown(MouseEvent mousey) {
-							System.out.println("view maze :: mouse is clicked");
-						
-							mouseCurXpos = mousey.x;
-							mouseCurYpos = mousey.y;
-						    System.out.println("mouse curr x : " + mouseCurXpos + " mouse curr y : " + mouseCurYpos );
-							
-						}
-			
-						public void mouseUp(MouseEvent mousey) {
-							// all the movement occurs when the mouse is released ...
-							System.out.println("mouse is released");
-							mouseNewXpos = mousey.x;
-							mouseNewYpos = mousey.y;
-							System.out.println("mouse last place (" + mouseCurXpos + "," + mouseCurYpos + ")"  );
-							System.out.println("mouse new  place (" + mouseNewXpos + "," + mouseNewYpos + ")"  );
-							if ( (mouseNewXpos > 0) && (mouseNewXpos < 29 )&& (mouseNewYpos < 0)) {
-								//this means we move up ...
-								setChanged();
-								userCommand=SWT.ARROW_UP;
-								notifyObservers();
-							}
-							else if ((mouseNewXpos < 0) && (mouseNewYpos > 0) && (mouseNewYpos < 30)){
-								//this means we move left ..
-								setChanged();
-								userCommand=SWT.ARROW_LEFT;
-								notifyObservers();
-							}
-							else if ((mouseNewXpos >= 30) && (mouseNewYpos > 0)&& (mouseNewYpos < 29)){
-								//this means we moved right
-								setChanged();
-								userCommand=SWT.ARROW_RIGHT;
-								notifyObservers();
-							}
-							else if ((mouseNewXpos > 0) && (mouseNewXpos < 29) && (mouseNewYpos >= 30)){
-								//this means we moved down
-								setChanged();
-								userCommand=SWT.ARROW_DOWN;
-								notifyObservers();
-							}
-							else if ((mouseNewXpos <0) && (mouseNewYpos < 0)){
-								//this means we moved diagonal left up 
-								setChanged();
-								horizental = -1;
-								vertical = 1;
-								notifyObservers("" + horizental + "," + vertical);
-							}
-							else if ((mouseNewXpos >30) && (mouseNewYpos < 0)){
-								//this means we moved diagonal right up 
-								setChanged();
-								horizental = 1;
-								vertical = 1;
-								notifyObservers("" + horizental + "," + vertical);
-							}
-							else if ((mouseNewXpos >30 ) && (mouseNewYpos > 30)){
-								//this means we moved diagonal right down
-								setChanged();
-								horizental = 1;
-								vertical = -1;
-								notifyObservers("" + horizental + "," + vertical);
-							}
-							else if ((mouseNewXpos <0) && (mouseNewYpos >30)){
-								//this means we moved diagonal left down 
-								setChanged();
-								horizental = -1;
-								vertical = -1;
-								notifyObservers("" + horizental + "," + vertical);
-							}
-							 
-						}
+				case SWT.ARROW_UP:
+					vertical++;
+					if (!upPressed) {
+						pressed++;
+						upPressed = true;
+					}
+					break;
+				case SWT.ARROW_DOWN:
+					vertical--;
+					if (!downPressed) {
+						pressed++;
+						downPressed = true;
+					}
+					break;
+				case SWT.ARROW_RIGHT:
+					horizental++;
+					if (!rightPressed) {
+						pressed++;
+						rightPressed = true;
+					}
+					break;
+				case SWT.ARROW_LEFT:
+					horizental--;
+					if (!leftPressed) {
+						pressed++;
+						leftPressed = true;
+					}
+					break;
 
-						@Override
-						public void mouseDoubleClick(MouseEvent arg0) {
-						}
-				
-					 	});	
+				default:
+					break;
+				}
 			}
-		
-	    shell.open();
-	}   
-	
+		});
+
+		// mouse event listener
+		for (int i = 0; i < rows; i++)
+			for (int j = 0; j < cols; j++) {
+				board.states[i][j].addMouseListener(new MouseListener() {
+					int mouseCurXpos;
+					int mouseCurYpos;
+					int mouseNewXpos;
+					int mouseNewYpos;
+
+					@Override
+					public void mouseDown(MouseEvent event) {
+						mouseCurXpos = event.x;
+						mouseCurYpos = event.y;
+						System.out.println("mouse Pressed. X= " + mouseCurXpos + " Y= " + mouseCurYpos);
+
+					}
+
+					@Override
+					public void mouseUp(MouseEvent event) {
+						mouseNewXpos = event.x;
+						mouseNewYpos = event.y;
+						Rectangle stateBound = ((State) event.widget).getBounds();
+						System.out.println("mouse Released. X= " + mouseNewXpos + " Y= " + mouseNewYpos);
+
+						if ((mouseNewXpos < 0) && (mouseNewYpos < 0)) { // Diagonal Left up
+							setChanged();
+							horizental = -1;
+							vertical = 1;
+							notifyObservers("" + horizental + "," + vertical);
+						} else if ((mouseNewXpos > stateBound.width) && (mouseNewYpos < 0)) { // Diagonal Right up
+							setChanged();
+							horizental = 1;
+							vertical = 1;
+							notifyObservers("" + horizental + "," + vertical);
+						} else if ((mouseNewXpos > stateBound.width) && (mouseNewYpos > stateBound.height)) { // Diagonal Right Down
+							setChanged();
+							horizental = 1;
+							vertical = -1;
+							notifyObservers("" + horizental + "," + vertical);
+						} else if ((mouseNewXpos < 0) && (mouseNewYpos > stateBound.height)) { // Diagonal Left Down
+							setChanged();
+							horizental = -1;
+							vertical = -1;
+							notifyObservers("" + horizental + "," + vertical);
+						} else if (mouseNewYpos < 0) { // up
+							setChanged();
+							userCommand = SWT.ARROW_UP;
+							notifyObservers();
+						} else if (mouseNewXpos < 0) { // Left
+							setChanged();
+							userCommand = SWT.ARROW_LEFT;
+							notifyObservers();
+						} else if (mouseNewXpos >= stateBound.width) { // Right
+							setChanged();
+							userCommand = SWT.ARROW_RIGHT;
+							notifyObservers();
+						} else if (mouseNewYpos >= stateBound.height) { // Down
+							setChanged();
+							userCommand = SWT.ARROW_DOWN;
+							notifyObservers();
+						}
+					}
+
+					@Override
+					public void mouseDoubleClick(MouseEvent arg0) {
+					}
+
+				});
+			}
+
+		shell.open();
+	}
+
 	@Override
-	public void run() { 		
+	public void run() {
 		initComponents();
 		setChanged();
 		notifyObservers();
 		shell.forceFocus();
-		while(!shell.isDisposed()){
-			if(!display.readAndDispatch()){ 
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
 				display.sleep();
 			}
 		}
 		display.dispose();
 	}
-	
-		
+
 	@Override
 	/*
-	 * Display the Board with the given data
-	 */
-	public void displayData(int[][] data) {		
+	* Display the Board with the given data
+	*/
+	public void displayData(int[][] data) {
 		board.setBoard(data);
 		board.redraw();
-				
+
 	}
 
 	/*
 	 *  Get user command
 	 */
 	@Override
-	public int getUserCommand() {		
+	public int getUserCommand() {
 		return userCommand;
 	}
-	
+
 	public void setUserCommand(int command) {
 		this.userCommand = command;
 	}
+
 	/*
 	 */
 	public void gameOver() {
 	}
-	
+
 	/*
 	 * Initialize Board canvas
 	 */
-	private void initializeBoard() {		
-		board = new Board(shell, SWT.BORDER,rows,cols,SWT.NONE);		
-		board.setLayoutData(new GridData(SWT.FILL,SWT.FILL,true,true, 1,5));
-		board.setBackground(display.getSystemColor(SWT.COLOR_WHITE));	
+	private void initializeBoard() {
+		board = new Board(shell, SWT.BORDER, rows, cols, SWT.NONE);
+		board.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 5));
+		board.setBackground(display.getSystemColor(SWT.COLOR_WHITE));
 		shell.forceFocus();
 	}
-	
+
 	/*
 	 * Initialize Menu buttons 
 	 */
-	private void initializeMenu() {	    
-		//Defines Menu
+	private void initializeMenu() {
+		// Defines Menu
 		Menu menuBar = new Menu(shell, SWT.BAR);
-	    MenuItem cascadeFileMenu = new MenuItem(menuBar, SWT.CASCADE);
-	    
-	    //File menu
-	    cascadeFileMenu.setText("&File");	        
-	    Menu fileMenu = new Menu(shell, SWT.DROP_DOWN);
-	    cascadeFileMenu.setMenu(fileMenu);
-	    MenuItem saveItem = new MenuItem(fileMenu,SWT.PUSH);
-	    saveItem.setText("&Save");
-	    MenuItem loadItem = new MenuItem(fileMenu,SWT.PUSH);
-	    loadItem.setText("&Load");
-	    MenuItem exitItem = new MenuItem(fileMenu, SWT.PUSH);
-	    exitItem.setText("&Exit");
-	    
-	    //Edit menu
-	    MenuItem cascadeEditMenu = new MenuItem(menuBar,SWT.CASCADE);
-	    cascadeEditMenu.setText("&Edit");
-	    Menu editMenu = new Menu(shell, SWT.DROP_DOWN);
-	    cascadeEditMenu.setMenu(editMenu);
-	    MenuItem restartItem = new MenuItem(editMenu,SWT.PUSH);
-	    restartItem.setText("&Restart");
-	    MenuItem undoItem = new MenuItem(editMenu,SWT.PUSH);
-	    undoItem.setText("&Undo");	    
-	    shell.setMenuBar(menuBar);
-	    //Defines the exit button
-	    exitItem.addSelectionListener(new SelectionAdapter() {
-	    	@Override
-	    	public void widgetSelected(SelectionEvent e) {
-	    		shell.getDisplay().dispose();
-	    		System.exit(0);
-	    	}
-	    });		
+		MenuItem cascadeFileMenu = new MenuItem(menuBar, SWT.CASCADE);
+
+		// File menu
+		cascadeFileMenu.setText("&File");
+		Menu fileMenu = new Menu(shell, SWT.DROP_DOWN);
+		cascadeFileMenu.setMenu(fileMenu);
+		MenuItem saveItem = new MenuItem(fileMenu, SWT.PUSH);
+		saveItem.setText("&Save");
+		MenuItem loadItem = new MenuItem(fileMenu, SWT.PUSH);
+		loadItem.setText("&Load");
+		MenuItem exitItem = new MenuItem(fileMenu, SWT.PUSH);
+		exitItem.setText("&Exit");
+
+		// Edit menu
+		MenuItem cascadeEditMenu = new MenuItem(menuBar, SWT.CASCADE);
+		cascadeEditMenu.setText("&Edit");
+		Menu editMenu = new Menu(shell, SWT.DROP_DOWN);
+		cascadeEditMenu.setMenu(editMenu);
+		MenuItem restartItem = new MenuItem(editMenu, SWT.PUSH);
+		restartItem.setText("&Restart");
+		MenuItem undoItem = new MenuItem(editMenu, SWT.PUSH);
+		undoItem.setText("&Undo");
+		shell.setMenuBar(menuBar);
+		// Defines the exit button
+		exitItem.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				shell.getDisplay().dispose();
+				System.exit(0);
+			}
+		});
 	}
-	
+
 	/*
 	 * Initialize Shell buttons
 	 */
-	private void initializeButtons()
-	{
-		//Defines undo button
-	    Button undoButton=new Button(shell, SWT.PUSH);
+	private void initializeButtons() {
+		// Defines undo button
+		Button undoButton = new Button(shell, SWT.PUSH);
 		undoButton.setText("Undo Move");
-		undoButton.setLayoutData(new GridData(SWT.NONE,SWT.NONE,false,false,1,1));
+		undoButton.setLayoutData(new GridData(SWT.NONE, SWT.NONE, false, false, 1, 1));
 		undoButton.addSelectionListener(new SelectionListener() {
 			@Override
 			public void widgetDefaultSelected(SelectionEvent arg0) {
 			}
 
 			@Override
-			public void widgetSelected(SelectionEvent arg0) {				
-				userCommand=11;
+			public void widgetSelected(SelectionEvent arg0) {
+				userCommand = 11;
 				setChanged();
-				notifyObservers();	
+				notifyObservers();
 				shell.forceFocus();
 			}
 		});
-		
-		//Defines restart Button
-		Button restartButton=new Button(shell, SWT.PUSH);
+
+		// Defines restart Button
+		Button restartButton = new Button(shell, SWT.PUSH);
 		restartButton.setText("Restart Game");
-		restartButton.setLayoutData(new GridData(SWT.NONE,SWT.NONE,false,false,1,1));
+		restartButton.setLayoutData(new GridData(SWT.NONE, SWT.NONE, false, false, 1, 1));
 		restartButton.addSelectionListener(new SelectionListener() {
 			@Override
 			public void widgetDefaultSelected(SelectionEvent arg0) {
 			}
 
 			@Override
-			public void widgetSelected(SelectionEvent arg0) {	
+			public void widgetSelected(SelectionEvent arg0) {
 				setUserCommand(2);
 				System.out.println("user restart game and sent command is=" + getUserCommand());
 				setChanged();
-				notifyObservers();			
+				notifyObservers();
 				shell.forceFocus();
 			}
 		});
-		
-		Button loadButton=new Button(shell, SWT.PUSH);
+
+		Button loadButton = new Button(shell, SWT.PUSH);
 		loadButton.setText("Load Game");
-		loadButton.setLayoutData(new GridData(SWT.NONE,SWT.NONE,false,false,1,1));
+		loadButton.setLayoutData(new GridData(SWT.NONE, SWT.NONE, false, false, 1, 1));
 		loadButton.addSelectionListener(new SelectionListener() {
 
-			@Override	
+			@Override
 			public void widgetDefaultSelected(SelectionEvent arg0) {
 				// TODO Auto-generated method stub
 			}
-			
+
 			public void widgetSelected(SelectionEvent arg0) {
-				userCommand=3;
+				userCommand = 3;
 				setChanged();
 				notifyObservers();
-				shell.forceFocus();					
+				shell.forceFocus();
 			}
 		});
-		Button saveButton=new Button(shell, SWT.PUSH);
-		saveButton.setText("Save Game");	
-		saveButton.setLayoutData(new GridData(SWT.NONE,SWT.NONE,false,false,1,1));
+		Button saveButton = new Button(shell, SWT.PUSH);
+		saveButton.setText("Save Game");
+		saveButton.setLayoutData(new GridData(SWT.NONE, SWT.NONE, false, false, 1, 1));
 		saveButton.addSelectionListener(new SelectionListener() {
 			@Override
 			public void widgetDefaultSelected(SelectionEvent arg0) {
-				// TODO Auto-generated method stub				
+				// TODO Auto-generated method stub
 			}
 
 			@Override
-			public void widgetSelected(SelectionEvent arg0) {				
-				userCommand=44;
+			public void widgetSelected(SelectionEvent arg0) {
+				userCommand = 44;
 				setChanged();
 				notifyObservers();
-				shell.forceFocus();			}
+				shell.forceFocus();
+			}
 		});
 	}
-	
 
 	@Override
 	public void undoEnd() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void displayScore(int scr) {
 		score.setText("Score " + scr);
 	}
-	
+
 	/*
 	 * Display Game Won board 
 	 */
 	@Override
-	public void gameWon() {		
-		MessageBox end = new MessageBox(shell,SWT.ICON_QUESTION | SWT.YES | SWT.NO);		
+	public void gameWon() {
+		MessageBox end = new MessageBox(shell, SWT.ICON_QUESTION | SWT.YES | SWT.NO);
 		end.setMessage("Your Are the MazeMaster,game won. do you wanna play another one?");
 		end.setText("GAME WON");
 		int response = end.open();
 		if (response == SWT.NO) {
 			display.dispose();
 			System.exit(0);
-		}
-		else
-		{
+		} else {
 			setUserCommand(2);
 			setChanged();
 			notifyObservers();
 		}
-		
+
 	}
 
 	/*
 	 * Display Game Won board 
 	 */
 	@Override
-	public void gameFinish() {		
-		MessageBox end = new MessageBox(shell,SWT.ICON_QUESTION | SWT.YES | SWT.NO);		
+	public void gameFinish() {
+		MessageBox end = new MessageBox(shell, SWT.ICON_QUESTION | SWT.YES | SWT.NO);
 		end.setMessage("You got to the END point but with More moves then possible! do you wanna play another one?");
 		end.setText("GAME FINISH");
 		int response = end.open();
 		if (response == SWT.NO) {
 			display.dispose();
 			System.exit(0);
-		}
-		else
-		{
+		} else {
 			setUserCommand(2);
 			setChanged();
 			notifyObservers();
 		}
-		
+
 	}
-	
+
 }
