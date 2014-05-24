b6f2b3598a9dad9430c40bab29eb4f4d5496f5b7
David Feldstein <david.f@fiverr.com>
2014-05-24 17:37:06 +0300

    [new] created server gui

--- src/server/runServer.java
+++ src/server/runServer.java
@@ -1,9 +1,11 @@
 package server;
 
+import java.rmi.AccessException;
 import java.rmi.AlreadyBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+
 import common.Constants;
 
 
@@ -11,31 +13,13 @@ public class runServer {
 
 	/**
 	 * @param args
+	 * @throws AccessException 
 	 * @throws RemoteException 
 	 * @throws AlreadyBoundException 
 	 */
-	public static void main(String[] args) throws RemoteException, AlreadyBoundException {
-		RemoteImpl2048 imp2048 = new RemoteImpl2048();
-		RemoteImplMaze impMaze = new RemoteImplMaze();
-		// Creation of Registry 
-		Registry registry=null;
-		try {
-			registry = LocateRegistry.createRegistry(Constants.RMI_PORT);
-			System.out.println("java RMI registry created.");
-			
-			//Server2048 binding
-			registry.bind("Server2048", imp2048);
-			System.out.println("2048 app bounded to server on port " + Constants.RMI_PORT);
-			
-			//ServerMaze binding		
-			registry.bind("ServerMaze", impMaze);
-			System.out.println("Maze app bounded to server on port " + Constants.RMI_PORT);		
-
-		}catch (RemoteException e) {
-			System.out.println("Registry already created on Port " + Constants.RMI_PORT);			
-		}		
-
-		
+	public static void main(String[] args) throws AccessException {
+		viewServer serverUi = new viewServer();
+		serverUi.run();
 	}
-
 }
+
